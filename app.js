7eee37e8863f13b33ca2dd7ce0e1e49ceb4c3c91
shazzaz1987 <shamim1987@gmail.com>
2020-04-17 18:03:40 -0400

    final commit

--- app.js
+++ app.js
@@ -7,39 +7,39 @@ var flash = require('connect-flash')
 var session = require('express-session')
 var passport = require('passport')
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var helmet = require('helmet');
 var MongoStore = require('connect-mongo')(session)
-var methodOverride = require('method-override')
-var dotenv = require('dotenv')
-var compression = require('compression');
-var mongoose = require('mongoose')
+//var methodOverride = require('method-override')
+//var dotenv = require('dotenv')
+var compression = require('compression')
+var mongoose=require('mongoose')
 mongoose.Promise = global.Promise
 
 /*
  Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.' + process.env.NODE_ENV })
-var dev_db_url = "mongodb+srv://shamim1987:<Gekmui10>@cluster0-9f1tg.gcp.mongodb.net/test?retryWrites=true&w=majority";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//dotenv.load({ path: '.env.' + process.env.NODE_ENV })
+var dev_db_url = 'mongodb+srv://shamim1987:Gekmui10@cluster0-9f1tg.gcp.mongodb.net/hello?retryWrites=true&w=majority'
+var mongoDB =  dev_db_url
+mongoose.connect(mongoDB)
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 
 
 //mongoose.connect(process.env.MONGO_URI)
-app.use(methodOverride)
+//app.use(methodOverride)
 app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 app.use(layout)
 app.use(compression()) //Compress all routes
 app.use(session({
-  secret: process.env.EXPRESS_SECRET,
+  secret: "my secret",
   resave: true,
   saveUninitialized: true,
   store: new MongoStore({
-    url: process.env.MONGO_URI,
+    url: "mongodb+srv://shamim1987:Gekmui10@cluster0-9f1tg.gcp.mongodb.net/hello?retryWrites=true&w=majority",
     autoReconnect: true
   })
 }))
@@ -49,7 +49,7 @@ app.use(passport.session())
 app.use(flash())
 app.use(helmet())
 // serve static files
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')))
 
 
@@ -70,5 +70,7 @@ app.use('/', usersRoutes)
 // app.use('/api/users', usersAPIRoutes)
 app.use('/', commentfrontendRoutes)
 
+module.exports = app;
 app.listen(process.env.PORT || 4000)
 console.log('Server started')
+
